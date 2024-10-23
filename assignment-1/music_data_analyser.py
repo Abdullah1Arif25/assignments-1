0faff19459bad8b9d442f8c8852588e0fa744b0c
Muhammad Abdullah Arif <abdullah1arif25@gmail.com>
2024-10-23 20:23:18 +0200

    Update music_data_analyser.py

--- assignment-1/music_data_analyser.py
+++ assignment-1/music_data_analyser.py
@@ -5,14 +5,17 @@ import json
 with open('assignment-1/database/spotify.json', mode= 'r') as artist_data:
     data = json.load(artist_data)
     items = data['items']
+    previous = data['next']
+
+    print(data.values())
     
     artist_name = items[0]['artists'][0]['name']
     print(f"{artist_name:.>5}")
 
-    for i in range(0, 10):
+    for i in range(0, 20):
         number_of_tracks_in_album = items[i]['total_tracks']
         artist_name = items[i]['artists'][0]['name']
         album_name = items[i]['name']
-        print(f"Name of The Album: {album_name:.>4}\n Number of Tracks: {number_of_tracks_in_album:.>4}")
+        print(f"Album: \t{album_name:.^4}\n Tracks: {number_of_tracks_in_album:}")
 
 
